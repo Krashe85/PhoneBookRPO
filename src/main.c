cd52edc64370313ee4800df74355eefc6d967527
Egor Golikov <53827497+Kimepl@users.noreply.github.com>
2020-11-26 21:27:43 +0300

    Add files via upload
    trsh

--- src/main.c
+++ src/main.c
@@ -1,19 +1,24 @@
 #include<stdio.h>
 #include"db/fakeBaseEmulator.h"
 #include"phoneProfile/phoneProfile.h"
+#include <string.h>
 
 
 int main(){
     fakeBase *db = initializationFakeDataBase();
-    while 1:
-        int userEnter = 0;
+    while (1);
+        int userEnter;
         printf("Выберете дейстиве:\n 1.Добавить номер\n Ваш выбор: ");
         scanf("%d", &userEnter);    
         switch (userEnter)
         {
-        case 1:
-            char *name, phone;
-            phoneProfile *newPhoneProfile = initializitePhoneProfile(name, phone);
+        case 1: ;
+            char name[]= "qweqwe";
+            char phone[]= "qweqwe";
+            //char name[500];
+            //char phone[500];
+            // scanf ("%s %s", &name, &phone);
+            phoneProfile *newPhoneProfile = initializitePhoneProfile(&name, &phone);
             fakeAdd(db, newPhoneProfile);
             printf("Профиль успешно добавлен!\n");
             printPhoneProfile(newPhoneProfile);
@@ -22,7 +27,7 @@ int main(){
             prrint("Покеда!");
             exit(0);
         default:
-            printf("Неизвестное действие\n")
+            printf("Неизвестное действие\n");
             break;
         }
     return 0;
