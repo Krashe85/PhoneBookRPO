6804778e398a48c01325078a74625ae1653128ca
Egor Skorikov <skorikov.egor@ya.ru>
2020-11-26 20:46:03 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,29 @@
+#include<stdio.h>
+#include"db/fakeBaseEmulator.h"
+#include"phoneProfile/phoneProfile.h"
+
 
 int main(){
+    fakeBase *db = initializationFakeDataBase();
+    while 1:
+        int userEnter = 0;
+        printf("Выберете дейстиве:\n 1.Добавить номер\n Ваш выбор: ");
+        scanf("%d", &userEnter);    
+        switch (userEnter)
+        {
+        case 1:
+            char *name, phone;
+            phoneProfile *newPhoneProfile = initializitePhoneProfile(name, phone);
+            fakeAdd(db, newPhoneProfile);
+            printf("Профиль успешно добавлен!\n");
+            printPhoneProfile(newPhoneProfile);
+            break;
+        case 10:
+            prrint("Покеда!");
+            exit(0);
+        default:
+            printf("Неизвестное действие\n")
+            break;
+        }
     return 0;
 }
